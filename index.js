f453a4759e9b7e5e63c507e3cd43bb53d70c7867
rabby-web <rabby.webdeveloper@gmail.com>
2023-10-20 00:15:54 +0600

    env file push

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-
-// digitalStore
-// 2ySH3MITYrnTNG0M
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://digitalStore:2ySH3MITYrnTNG0M@cluster1.gnm5d1v.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.gnm5d1v.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
